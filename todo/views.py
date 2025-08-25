5c18916bbe310c52794977c3a64495f447703861
Oussamaht16 <oussamaelheitpersonal@gmail.com>
2025-08-25 18:21:40 +0100

    final commit

--- todo/views.py
+++ todo/views.py
@@ -1,9 +1,44 @@
+from encodings.punycode import T
 from django.http import HttpResponse
-from django.shortcuts import render , redirect
+from django.shortcuts import get_object_or_404, render , redirect
 from .models import Task
 # Create your views here.
 
 def addtask(request):
     taskr = request.POST['task'] #hadi 'task'hia nafsha li rahi dakhal name dakhal input dakhal html
     Task.objects.create(task=taskr  )# lokhrin madarnahomch psq wahad rahi par defaut o zoj li b9aw auto generated
+    #print(request.POST)
+    return redirect('home')
+
+
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task , pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task , pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('home')
+    else:
+      context = {
+          'get_task':get_task,
+                }  
+    return render(request, 'edit_task.html' , context)
+
+
+def delete_task(request, id):
+    task = get_object_or_404(Task , id=id )
+    task.delete()
     return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -6,7 +6,9 @@ from todo.models import Task
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-updated_at')
     #hna mdari nst3mlo get mais hna edna li fihom false o true hna habina y3tina li rahom false
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-updated_at')
     context = {
         'tasks' : tasks,
+        'completed_tasks' : completed_tasks,
     }
     return render(request, 'home.html' , context)
